581ecc538bbc175867d3fab4336073e4276a4d95
sanjoyk <sanjoyksardar01@gmail.com>
2018-07-02 23:48:57 +0530

    add rules in index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+module.export={
+   rules:{
+    "indent": [
+        "error",
+        4
+    ],
+    "linebreak-style": [
+        "error",
+        "unix"
+    ],
+    "quotes": [
+        "error",
+        "double"
+    ],
+    "semi": [
+        "error",
+        "always"
+    ]
+   } 
+}
